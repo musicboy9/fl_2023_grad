7829aeed788c8c4c93f571a187d90bbdbbe9c9df
Yeongmin Koh <musicboy9@snu.ac.kr>
2023-05-12 21:25:58 +0900

    Fix visual feature

--- app/client/custom_torch.py
+++ app/client/custom_torch.py
@@ -24,31 +24,46 @@ class CustomTorch:
 
     def train(self, trainloader, epochs):
         """Train the model on the training set."""
+        self.status_dict[(self.client_id, "status")] = "training"
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.SGD(self.net.parameters(), lr=0.001, momentum=0.9)
         for _ in range(epochs):
-            self.status_dict[self.client_id] = [date.now(), 0]
             i = 0
             data_len = len(trainloader)
+            print(data_len)
+            self.status_dict[(self.client_id, "train_result")] = [date.now(), 0, data_len]
+            self.status_dict[(self.client_id, "data_len")] = data_len
             for images, labels in trainloader:
                 i += 1
-                self.status_dict[self.client_id] = [date.now(), i/data_len]
-
+                self.status_dict[(self.client_id, "train_result")] = [date.now(), i, data_len]
                 optimizer.zero_grad()
                 criterion(self.net(images.to(self.device)), labels.to(self.device)).backward()
                 optimizer.step()
 
+        self.status_dict[(self.client_id, "status")] = "idle"
+        self.status_dict[(self.client_id, "data_len")] = 0
+
     def test(self, testloader):
         """Validate the model on the test set."""
+        self.status_dict[(self.client_id, "status")] = "testing"
         criterion = torch.nn.CrossEntropyLoss()
         correct, loss = 0, 0.0
         with torch.no_grad():
+            i = 0
+            data_len = len(testloader.dataset)
+            self.status_dict[(self.client_id, "eval_result")] = [date.now(), 0, data_len]
+            self.status_dict[(self.client_id, "data_len")] = data_len
             for images, labels in testloader:
+                i += 1
+                self.status_dict[(self.client_id, "eval_result")] = [date.now(), i, data_len]
                 outputs = self.net(images.to(self.device))
                 labels = labels.to(self.device)
                 loss += criterion(outputs, labels).item()
                 correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
         accuracy = correct / len(testloader.dataset)
+
+        self.status_dict[(self.client_id, "status")] = "idle"
+        self.status_dict[(self.client_id, "data_len")] = 0
         return loss, accuracy
 
     def load_data(self):
@@ -63,4 +78,5 @@ class CustomTorch:
         custom_data_test_len = int(len(testset) * self.data_size)
         custom_testset = torch.utils.data.random_split(testset, [custom_data_test_len, len(testset) - custom_data_test_len])[0]
 
+        self.status_dict[(self.client_id, "data_size")] = (self.data_size)
         return DataLoader(custom_trainset, batch_size=self.batch_size, shuffle=True), DataLoader(custom_testset)
--- app/viewer/progress_viewer.py
+++ app/viewer/progress_viewer.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QBrush, QColor
+from collections import defaultdict
+import time
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, status_dict):
@@ -7,48 +9,80 @@ class MyWidget(QtWidgets.QWidget):
 
         self.status_dict = status_dict
 
-        self.setGeometry(30,30,600,400)
-        self.pos1 = [0,0]
-        self.pos2 = [0,0]
+        self.__init_fetcher()
+        self.__init_geometry()
+
         self.show()
         self.showing = True
 
+        self.last_val_dict = defaultdict(int)
+
+        self.time = time.time()
+        self.time_set = False
+
+
+    def __init_geometry(self):
+        self.margin = 10
+
+        self.setGeometry(30, 30, 600, 400)
+        self.col_height = 50
+        self.max_width = self.geometry().width() - 2 * self.margin
+
+        self.pos_list = [[self.margin, self.margin],
+                         [self.margin, self.col_height + self.margin]]
+
+
+    def __init_fetcher(self):
         self.timer = QtCore.QTimer(self)
-        self.timer.setInterval(100)  # Throw event timeout with an interval of 1000 milliseconds
+        self.timer.setInterval(1)  # Throw event timeout with an interval of 1000 milliseconds
         self.timer.timeout.connect(self.fetch)  # each time timer counts a second, call self.blink
         self.color_flag = True
 
         self.timer.start()
 
     def paintEvent(self, event):
-        width = self.pos2[0]-self.pos1[0]
-        height = self.pos2[1] - self.pos1[1]
 
-        qp = QPainter()
-        qp.begin(self)
-        qp.drawRect(self.pos1[0], self.pos1[1], width, height)
-        qp.end()
 
-    def mousePressEvent(self, event):
-        self.pos1[0], self.pos1[1] = event.pos().x(), event.pos().y()
-        print("clicked")
+        for i in range(len(self.pos_list)):
+            pos = self.pos_list[i]
+            if ((i, "data_size") in self.status_dict):
+                if (i, "status") in self.status_dict and (i, "train_result") in self.status_dict:
+                    if self.status_dict[(i, "status")] == "training":
+                        if not self.time_set:
+                            self.time = time.time()
+                            self.time_set = True
 
-    def mouseReleaseEvent(self, event):
-        self.pos2[0], self.pos2[1] = event.pos().x(), event.pos().y()
-        print("released")
-        self.update()
+                        qp = QPainter()
+                        qp.begin(self)
+                        # qp.drawRect(pos[ 0 ], pos[ 1 ], self.max_width * self.status_dict[ (i, "data_size") ],
+                        #             self.col_height)
+                        current_width = self.max_width * (time.time() - self.time) / 30
+                        qp.drawRect(pos[ 0 ], pos[ 1 ], current_width, self.col_height)
+
+                        brush = QBrush()
+                        brush.setColor(QColor("#FFD141"))
+                        brush.setStyle(QtCore.Qt.BrushStyle.Dense1Pattern)
+                        qp.setBrush(brush)
+                        rect = QtCore.QRect(pos[0],
+                                            pos[1],
+                                            # int(
+                                            #     self.max_width *
+                                            #     self.status_dict[(i, "data_size")] *
+                                            #     (self.status_dict[(i, "train_result")][1] / self.status_dict[(i, "train_result")][2])
+                                            # ),
+                                            int(
+                                                current_width *
+                                                (self.status_dict[(i, "train_result")][1] / self.status_dict[(i, "train_result")][2])
+                                            ),
+                                            self.col_height)
+                        qp.drawRect(rect)
+                        qp.setPen(QtCore.Qt.black)
+                        qp.drawText(rect, QtCore.Qt.AlignCenter, str(self.status_dict[(i, "train_result")][1]) + "/" + str(self.status_dict[(i, "train_result")][2]))
+                        qp.end()
+
 
     @QtCore.pyqtSlot()
     def fetch(self):
-        try:
-            for key in self.status_dict.keys():
-                if len(str(self.status_dict[key])) > 10:
-                    print(str(self.status_dict[key])[:10])
-                else:
-                    print(self.status_dict[key])
-        except:
-            pass
-
         self.update()
 
     def getShowing(self):
